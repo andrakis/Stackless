d8ae0f1e72435f4083d2f635f598f114c9a2f8c4
Julian 'Andrakis' Thatcher <daedalus.raistlin@gmail.com>
2017-11-24 22:31:06 +1100

    More work in progress Stackless Scheme
    None of this is tested yet. Mostly theory, but it compiles.

--- Stackless/samples/Scheme.cpp
+++ Stackless/samples/Scheme.cpp
@@ -14,6 +14,9 @@ using namespace stackless;
 using namespace stackless::microthreading;
 using namespace stackless::timekeeping;
 
+namespace implementations {
+namespace scheme {
+
 // return given mumber as a string
 std::string str(long n) { std::ostringstream os; os << n; return os.str(); }
 
@@ -117,6 +120,10 @@ struct environment : public Environment<cells> {
 		return env_[var];
 	}
 
+	void define(const std::string & var, const cell &val) {
+		env_[var] = val;
+	}
+
 private:
 	map env_; // inner symbol->cell mapping
 	env_p outer_; // next adjacent outer env, or 0 if there are no further environments
@@ -124,9 +131,9 @@ private:
 
 // frame implementation
 struct SchemeFrame : public Frame<typename cell, typename std::string, typename environment> {
-	SchemeFrame(const cell &expression, env_p environment)
+	SchemeFrame(env_p environment)
 		: Frame(environment),
-		exp(expression),
+		exp(nil),
 		expressions(),
 		arguments(),
 		resolved_arguments(),
@@ -135,6 +142,12 @@ struct SchemeFrame : public Frame<typename cell, typename std::string, typename 
 		resolved(false),
 		subframe(nullptr),
 		subframe_mode(None)
+	{
+	}
+
+	// Calls above constructor
+	SchemeFrame(const cell &expression, env_p environment)
+		: SchemeFrame(environment)
 	{
 		expressions.push_back(exp);
 		exp_it = expressions.cbegin();
@@ -347,7 +360,6 @@ struct SchemeFrame : public Frame<typename cell, typename std::string, typename 
 };
 
 
-
 template<typename instruction::instruction Instruction> struct SchemeDispatcher {
 	static bool dispatch(SchemeFrame &frame, cells::const_iterator args) {
 		throw stackless::InvalidOperation<typename instruction::instruction, int, int>(Instruction, 0, 0);
@@ -362,7 +374,7 @@ template<> struct SchemeDispatcher<instruction::If> {
 		const cell &if_result = (test.val == "#t") ? conseq : alt;
 		switch (if_result.type) {
 		case Symbol:
-			frame.result = frame.env->find(if_result.val)[if_result.val];
+			frame.result = frame.lookup(if_result.val);
 			break;
 		case List:
 			if (if_result.list.empty()) {
@@ -395,18 +407,39 @@ template<> struct SchemeDispatcher<instruction::Begin> {
 	}
 };
 
+template<> struct SchemeDispatcher<instruction::Set> {
+	static bool dispatch(SchemeFrame &frame, cells::const_iterator it) {
+		const cell &var = *it; ++it;
+		const cell &val = *it; ++it;
+		frame.lookup(var.val) = frame.result = val;
+		return true;
+	}
+};
+
+template<> struct SchemeDispatcher<instruction::Define> {
+	static bool dispatch(SchemeFrame &frame, cells::const_iterator it) {
+		const cell &var = *it; ++it;
+		const cell &val = *it; ++it;
+		frame.env->define(var.val, val);
+		frame.result = val;
+		return true;
+	}
+};
+
 template<> struct SchemeDispatcher<instruction::Proc> {
 	static bool dispatch(SchemeFrame &frame, cells::const_iterator it) {
 		switch (frame.exp.type) {
-			// Proc: a builtin procedure in C++
-			// We call this immediately. If it blocks, that is up
-			// to the caller.
+		// Proc: a builtin procedure in C++
+		// We call this immediately. If it blocks, that is up
+		// to the caller.
 		case Proc:
 			frame.result = frame.exp.proc(frame.resolved_arguments);
+			// TODO: Support a proc that waits
+			// Move exp_it
 			return true;
-			// Lambda: a Scheme procedure
-			// We create a subframe to run the procedure, along with
-			// an environment with arguments set to correct values.
+		// Lambda: a Scheme procedure
+		// We create a subframe to run the procedure, along with
+		// an environment with arguments set to correct values.
 		case Lambda:
 		{
 			// Arguments
@@ -414,15 +447,15 @@ template<> struct SchemeDispatcher<instruction::Proc> {
 			cells arguments;
 			switch (arglist.type) {
 			case Symbol:
-			{
 				// Single argument, assign all args as list
 				arguments.push_back(arglist);
 				// Convert resolved arguments to single list
-				cells tmp(frame.resolved_arguments);
-				frame.resolved_arguments.clear();
-				frame.resolved_arguments.push_back(tmp);
+				{
+					cells tmp(frame.resolved_arguments);
+					frame.resolved_arguments.clear();
+					frame.resolved_arguments.push_back(tmp);
+				}
 				break;
-			}
 			case List:
 				// List of arguments
 				arguments = arglist.list;
@@ -435,10 +468,11 @@ template<> struct SchemeDispatcher<instruction::Proc> {
 			auto env_arg_it = arguments.cbegin();
 			auto res_arg_it = frame.resolved_arguments.cbegin();
 			for (; res_arg_it != arguments.cend(); ++env_arg_it, ++res_arg_it)
-				new_env->operator[](env_arg_it->val) = *res_arg_it;
+				new_env->define(env_arg_it->val, *res_arg_it);
 			// Create subframe
 			frame.subframe_mode = SchemeFrame::Procedure;
 			frame.subframe = new SchemeFrame(body, new_env);
+			// Don't move exp_it
 			return false;
 		}
 		default:
@@ -456,11 +490,30 @@ bool SchemeFrame::dispatchCall() {
 		return SchemeDispatcher<instruction::If>::dispatch(*this, it);
 	case instruction::Begin:
 		return SchemeDispatcher<instruction::Begin>::dispatch(*this, it);
+	case instruction::Set:
+		return SchemeDispatcher<instruction::Set>::dispatch(*this, it);
+	case instruction::Define:
+		return SchemeDispatcher<instruction::Define>::dispatch(*this, it);
 	case instruction::Proc:
 		return SchemeDispatcher<instruction::Proc>::dispatch(*this, it);
 	default:
 		return SchemeDispatcher<instruction::Invalid>::dispatch(*this, it);
 	}
+}
+
+struct SchemeImplementation : public Implementation<environment, SchemeFrame> {
+	SchemeImplementation(env_p _env) : Implementation(_env), frame(SchemeFrame(_env)) {
+
+	}
+private:
+	SchemeFrame frame;
+};
+
+cell eval(cell ins, env_p env) {
+	// create thread
+	// execute multithreading until thread resolved
+	// return frame result
+	return nil;
 }
 
 ////////////////////// built-in primitive procedures
@@ -566,4 +619,173 @@ void add_globals(environment & env)
 	env["-"] = cell(&proc_sub);      env["*"] = cell(&proc_mul);
 	env["/"] = cell(&proc_div);      env[">"] = cell(&proc_greater);
 	env["<"] = cell(&proc_less);     env["<="] = cell(&proc_less_equal);
+}
+////////////////////// parse, read and user interaction
+
+// convert given string to list of tokens
+std::list<std::string> tokenize(const std::string & str)
+{
+	std::list<std::string> tokens;
+	const char * s = str.c_str();
+	while (*s) {
+		while (*s == ' ')
+			++s;
+		if (*s == '(' || *s == ')')
+			tokens.push_back(*s++ == '(' ? "(" : ")");
+		else {
+			const char * t = s;
+			while (*t && *t != ' ' && *t != '(' && *t != ')')
+				++t;
+			tokens.push_back(std::string(s, t));
+			s = t;
+		}
+	}
+	return tokens;
+}
+
+// numbers become Numbers; every other token is a Symbol
+cell atom(const std::string & token)
+{
+	if (isdig(token[0]) || (token[0] == '-' && isdig(token[1])))
+		return cell(Number, token);
+	return cell(Symbol, token);
+}
+
+// return the Lisp expression in the given tokens
+cell read_from(std::list<std::string> & tokens)
+{
+	const std::string token(tokens.front());
+	tokens.pop_front();
+	if (token == "(") {
+		cell c(List);
+		while (tokens.front() != ")")
+			c.list.push_back(read_from(tokens));
+		tokens.pop_front();
+		return c;
+	} else
+		return atom(token);
+}
+
+// return the Lisp expression represented by the given string
+cell read(const std::string & s)
+{
+	std::list<std::string> tokens(tokenize(s));
+	return read_from(tokens);
+}
+
+// convert given cell to a Lisp-readable string
+std::string to_string(const cell & exp)
+{
+	if (exp.type == List) {
+		std::string s("(");
+		for (cell::iter e = exp.list.begin(); e != exp.list.end(); ++e)
+			s += to_string(*e) + ' ';
+		if (s[s.size() - 1] == ' ')
+			s.erase(s.size() - 1);
+		return s + ')';
+	} else if (exp.type == Lambda)
+		return "<Lambda>";
+	else if (exp.type == Proc)
+		return "<Proc>";
+	return exp.val;
+}
+
+// the default read-eval-print-loop
+void repl(const std::string & prompt, env_p env)
+{
+	for (;;) {
+		std::cout << prompt;
+		std::string line; std::getline(std::cin, line);
+		std::cout << to_string(eval(read(line), env)) << '\n';
+	}
+}
+
+int scheme_main()
+{
+	env_p global_env; add_globals(*global_env);
+	repl("90> ", global_env);
+	return 0;
+}
+
+void scheme_test() {
+	std::string line;
+	env_p env; add_globals(*env);
+	eval(read("(define multiply-by (lambda (n) (lambda (y) (* y n))))"), env);
+	eval(read("(define doubler (multiply-by 2))"), env);
+	cell result = eval(read("(doubler 4)"), env);
+	std::cout << to_string(result) << std::endl;
+}
+
+////////////////////// unit tests
+unsigned g_test_count;      // count of number of unit tests executed
+unsigned g_fault_count;     // count of number of unit tests that fail
+template <typename T1, typename T2>
+void test_equal_(const T1 & value, const T2 & expected_value, const char * file, int line)
+{
+	++g_test_count;
+	std::cerr
+		//<< file
+		<< '(' << line << ") : "
+		<< " expected " << expected_value
+		<< ", got " << value;
+	if (value != expected_value) {
+		++g_fault_count;
+		std::cerr << " - FAIL\n";
+	} else {
+		std::cerr << " - success\n";
+	}
+}
+// write a message to std::cout if value != expected_value
+#define TEST_EQUAL(value, expected_value) test_equal_(value, expected_value, __FILE__, __LINE__)
+// evaluate the given Lisp expression and compare the result against the given expected_result
+#define TEST(expr, expected_result) TEST_EQUAL(to_string(eval(read(expr), global_env)), expected_result)
+
+unsigned scheme_complete_test() {
+	env_p global_env; add_globals(*global_env);
+	// the 29 unit tests for lis.py
+	TEST("(quote (testing 1 (2.0) -3.14e159))", "(testing 1 (2.0) -3.14e159)");
+	TEST("(+ 2 2)", "4");
+	TEST("(+ (* 2 100) (* 1 10))", "210");
+	TEST("(if (> 6 5) (+ 1 1) (+ 2 2))", "2");
+	TEST("(if (< 6 5) (+ 1 1) (+ 2 2))", "4");
+	TEST("(define x 3)", "3");
+	TEST("x", "3");
+	TEST("(+ x x)", "6");
+	TEST("(begin (define x 1) (set! x (+ x 1)) (+ x 1))", "3");
+	TEST("((lambda (x) (+ x x)) 5)", "10");
+	TEST("(define twice (lambda (x) (* 2 x)))", "<Lambda>");
+	TEST("(twice 5)", "10");
+	TEST("(define compose (lambda (f g) (lambda (x) (f (g x)))))", "<Lambda>");
+	TEST("((compose list twice) 5)", "(10)");
+	TEST("(define repeat (lambda (f) (compose f f)))", "<Lambda>");
+	TEST("((repeat twice) 5)", "20");
+	TEST("((repeat (repeat twice)) 5)", "80");
+	TEST("(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))", "<Lambda>");
+	TEST("(fact 3)", "6");
+	//TEST("(fact 50)", "30414093201713378043612608166064768844377641568960512000000000000");
+	TEST("(fact 12)", "479001600"); // no bignums; this is as far as we go with 32 bits
+	TEST("(define abs (lambda (n) ((if (> n 0) + -) 0 n)))", "<Lambda>");
+	TEST("(list (abs -3) (abs 0) (abs 3))", "(3 0 3)");
+	TEST("(define combine (lambda (f)"
+		"(lambda (x y)"
+		"(if (null? x) (quote ())"
+		"(f (list (head x) (head y))"
+		"((combine f) (tail x) (tail y)))))))", "<Lambda>");
+	TEST("(define zip (combine cons))", "<Lambda>");
+	TEST("(zip (list 1 2 3 4) (list 5 6 7 8))", "((1 5) (2 6) (3 7) (4 8))");
+	TEST("(define riff-shuffle (lambda (deck) (begin"
+		"(define take (lambda (n seq) (if (<= n 0) (quote ()) (cons (head seq) (take (- n 1) (tail seq))))))"
+		"(define drop (lambda (n seq) (if (<= n 0) seq (drop (- n 1) (tail seq)))))"
+		"(define mid (lambda (seq) (/ (length seq) 2)))"
+		"((combine append) (take (mid deck) deck) (drop (mid deck) deck)))))", "<Lambda>");
+	TEST("(riff-shuffle (list 1 2 3 4 5 6 7 8))", "(1 5 2 6 3 7 4 8)");
+	TEST("((repeat riff-shuffle) (list 1 2 3 4 5 6 7 8))", "(1 3 5 7 2 4 6 8)");
+	TEST("(riff-shuffle (riff-shuffle (riff-shuffle (list 1 2 3 4 5 6 7 8))))", "(1 2 3 4 5 6 7 8)");
+	std::cout
+		<< "total tests " << g_test_count
+		<< ", total failures " << g_fault_count
+		<< "\n";
+	return g_fault_count ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+}
 }
